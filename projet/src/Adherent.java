149b2494bd61d951913759b2fada60ea7cf9ee1c
THOMAS <thomas.pernod@edu.ece.fr>
2026-03-30 11:11:25 +0200

    ajout de la structure du projet

---
+++ projet/src/Adherent.java
@@ -0,0 +1,15 @@
+public class Adherent extends Personne{
+    private String formule;
+    private int serviette;
+    private int cadena;
+    private int cle;
+
+
+    public Adherent(){
+        String formule = "Liberté";
+        int serviette = 0;
+        int cadena = 0;
+        int cle = 0;
+    }
+
+}
---
+++ projet/src/Personne.java
@@ -0,0 +1,16 @@
+public class Personne {
+    protected int id;
+    protected int age;
+    protected String nom;
+    protected String prenom;
+    protected int tel;
+    protected String email;
+    protected String mdp;
+
+
+    public void scannerBadge(){
+
+    }
+
+
+}
